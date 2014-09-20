53368dea9b8eb4913f91f254b21117462db12854
Ben Collier <bmcollier@gmail.com>
2014-09-20 14:00:39 +0100

    Added ball movement routine

--- paddler.py
+++ paddler.py
@@ -1,31 +1,54 @@
+from time import sleep
 import curses
+
+def check_keys():
+    pass
+    
+def plot_paddle():
+    pass
+    
+def check_paddle_collision():
+    pass
+
+def check_wall_collision():
+    pass
+
+def move_ball():
+    #stdscr.addstr(ball_location[1],ball_location[0], ' ') # erase ball
+    ball_location[0] = ball_location[0] + ball_movement[0]
+    ball_location[1] = ball_location[1] + ball_movement[1]
+    stdscr.addstr(ball_location[1],ball_location[0], "o") # plot ball
+    sleep(ball_movement[2])
 
 stdscr = curses.initscr()
 curses.noecho()
-
+curses.curs_set(0)
 quit = False
-ball_movement = [0,-1, 100] # x movement, y movement, delay (milliseconds)
+ball_movement = [0,-1, 1] # x movement, y movement, delay (seconds)
+ball_location = [40,12]
 
 while quit == False:
+    
+    input_command = None
+    collide = None
+    
     input_command = check_keys()
     if input_command:
         plot_paddle(input_command)
     check_paddle_collision() # is ball about to hit a paddle?
-    collide = check wall_collision() # is ball about to hit a wall?
-    if collide == True:
+    collide = check_wall_collision() # is ball about to hit a wall?
+    if collide:
         quit = True
     else:
         move_ball()
+    stdscr.refresh()
 
-def check_keys():
-    
-def plot_paddle():
-    
-def check_paddle_collision():
+curses.nocbreak()
+stdscr.keypad(0)
+curses.echo()
+curses.endwin()
 
-def check_wall_collision():
-
-#pad = curses.newpad(100, 100)
+#
 #  These loops fill the pad with letters; this is
 # explained in the next section
 #for y in range(0, 100):
