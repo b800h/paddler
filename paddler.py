5801703e40ea8b6c56d1c5849a695369904b97b2
Ben Collier <bmcollier@gmail.com>
2014-09-20 13:11:32 +0100

    Added working pause at end of program for test.

--- paddler.py
+++ paddler.py
@@ -1,16 +1,19 @@
 import curses
 
 stdscr = curses.initscr()
-curses.noecho()
+curses.echo()
 
 pad = curses.newpad(100, 100)
 #  These loops fill the pad with letters; this is
 # explained in the next section
 for y in range(0, 100):
     for x in range(0, 100):
-        pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+        try:
+            pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+        except curses.error:
+            pass
 
 #  Displays a section of the pad in the middle of the screen
 pad.refresh(0,0, 5,5, 20,75)
 
-pause()
+raw_input()
