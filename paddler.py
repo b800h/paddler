4e3c83b9e6c22f0026b902fa6afc6250d9005abb
Ben Collier <bmcollier@gmail.com>
2014-09-20 12:53:31 +0100

    Added curses test routine

--- paddler.py
+++ paddler.py
@@ -1,4 +1,16 @@
-import unicurses as curses
+import curses
 
 stdscr = curses.initscr()
 curses.noecho()
+
+pad = curses.newpad(100, 100)
+#  These loops fill the pad with letters; this is
+# explained in the next section
+for y in range(0, 100):
+    for x in range(0, 100):
+        pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+
+#  Displays a section of the pad in the middle of the screen
+pad.refresh(0,0, 5,5, 20,75)
+
+pause()
